16cf82141675a3f1a58f79f1fc031566b90b1261
Ingrid04 <ingridfmelgar@gmail.com>
2023-05-31 12:55:29 -0500

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Slides from './Components/Images/Slides';
 
 
 function App() {
-
-  // const handleScroll = (e) => {
-  //   console.log(e, 'event')
-  //    window.scrollBy(e.deltaY, 0)
-  //  }
-  // window.addEventListener('wheel', handleScroll);
 
   return (
     <div className="App">
